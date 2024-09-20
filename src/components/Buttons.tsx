835be30ffc53b307ff4ef533d32d2467bfd8116b
rok <rexblanegamer@gmail.com>
2024-09-20 10:42:50 -0300

    fix: :bug: Fixing McButton properties

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -8,11 +8,11 @@ export default function McButton(props: { className?: string, onClick?: () => vo
 	return (
 
 			<button
-				className="btn min-w-5 min-h-5"
+				className={clsx("btn min-w-5 min-h-5", props.className)}
 				type="button"
 				onClick={props.onClick}
 			>
-				<span className={clsx("font-minecraft flex items-center justify-around flex-1 border-b-3 border-gray-800 shadow-white shadow-inner p-2", props.className)}>
+				<span className="font-minecraft flex items-center justify-around flex-1 border-b-3 border-gray-800 shadow-white shadow-inner p-2">
 					<div className="m-3">
 					{props.children}
 					</div>
